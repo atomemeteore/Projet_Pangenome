a40c74ad7dda06c06646db240f774df2d3225986
nguyeho3 <hoang.nguyen@univ-rouen.fr>
2025-01-18 00:12:10 +0100

    Added concatenate.py, modified extraction_chromosome.py and workflow.ipynb

---
+++ concatenate.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from collections import defaultdict
+
+# Répertoire contenant les fichiers .fasta
+chemin = Path("/home/nguyeho3/Documents/Github_Pangenome/Projet_Pangenome/Test_Nextflow/Chromosomes")
+
+# Dictionnaire pour regrouper les fichiers par leur suffixe
+groupes_fichiers = defaultdict(list)
+
+# Dictionnaires pour les groupes de contenu
+groupes_contenu = {
+    "C1": [],
+    "C2": [],
+    "C3": [],
+    "C4": [],
+    "C5": [],
+    "C6": [],
+    "C7": [],
+    "C8": [],
+    "C9": [],
+    "00000010": []
+}
+
+# Parcourir les fichiers dans le répertoire
+for element in chemin.glob("*.fasta"):
+    with open(element, 'r') as chromosome:
+        contenu = chromosome.read()
+        for key in groupes_contenu.keys():
+            if key in contenu:
+                groupes_contenu[key].append(contenu)
+
+# Créer le répertoire de sortie s'il n'existe pas
+dossier = Path("/home/nguyeho3/Documents/Github_Pangenome/Projet_Pangenome/Test_Nextflow/Concat")
+if not dossier.exists():
+    dossier.mkdir(parents=True)
+
+# Écrire les fichiers concaténés
+for key, sequences in groupes_contenu.items():
+    if sequences:  # Vérifier qu'il y a des séquences à écrire
+        output_file = dossier / f"Concat_{key}.fasta"
+        with open(output_file, "w") as concat:
+            concat.write("\n".join(sequences))
+
+print("Concaténation terminée avec succès.")
---
+++ extraction_chromosome.py
@@ -0,0 +1,37 @@
+
+from Bio import SeqIO
+import os
+import re
+
+repertoire = "Projet_Pangenome/genomes"
+input_list = []
+
+for nom_fichier in os.listdir(repertoire):
+    chemin_complet = os.path.join(repertoire, nom_fichier)
+    input_list.append(chemin_complet)
+
+pattern = re.compile(r"(.+)\/genomes\/(.+)\.genome\.fasta")
+
+def extraction_chromosome():
+    for file in input_list:
+        print(file)
+        # Dossier de sortie
+        match = pattern.match(file)
+        if match:
+            print(match.group(2))                 
+            output_folder = f"Projet_Pangenome/Chromosomes_{match.group(2)}"
+            os.makedirs(output_folder, exist_ok=True)
+            # Lecture et extraction des séquences
+            for record in SeqIO.parse(file, "fasta"):
+                    # Construire un fichier de sortie basé sur l'identifiant
+                    output_file = os.path.join(output_folder,f"{record.id}.fasta")
+                    with open(output_file, "w") as f:
+                        SeqIO.write(record, f, "fasta")
+                    print(f"Séquence {record.id} sauvegardée dans {output_file}")
+    print("Extraction terminée.")
+
+def main():
+    extraction_chromosome()
+
+if __name__ == '__main__':
+    main()
